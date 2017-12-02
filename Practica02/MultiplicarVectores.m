b22e0da371da02a597632369b9db9f9cde4eb765
essalguero <essalguero@yahoo.es>
2017-12-02 22:27:09 +0100

    Add files via upload

--- Practica02/MultiplicarVectores.m
+++ Practica02/MultiplicarVectores.m
@@ -2,12 +2,15 @@ function resultado = MultiplicarVectores(P1, P2)
   
   m = eye(4);
   
-  for i = [1:4]
-    for j = [1:4]
-      m(i, j) = P1(i) * P2(j);
-    end
-  end
-  
+%  for i = [1:4]
+%    for j = [1:4]
+%      m(i, j) = P1(i) * P2(j);
+%    end
+%  end
+%  
+
+m = P1'.*P2;
+
   resultado = m;
   
 end
