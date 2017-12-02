07edcc8f2af1cd4ff62b25b01084641f102eb985
essalguero <essalguero@yahoo.es>
2017-12-02 21:36:25 +0100

    Add files via upload

--- Practica02/Problema10.m
+++ Practica02/Problema10.m
@@ -1,28 +1,13 @@
-% Calcula el plano PI dfinido por el punto (1; 1; -3) la recta dada por 
-% las ecuaciones x+y = 1 y x-z = 0. Calcula la intersección de la recta que 
-% pasa por los puntos (1; 0; -1) y (-3; -2; -3) con PI.
+% Realiza un programa .m en Octave que dadas 4 correspondencias calcule la 
+% homografía correspondiente, y comprueba que para 
+% xi =  {(1, 0, 1),(0, 1, 1),(0, 0, 1), (1, 1, 1)} y 
+% x'i = {(2, 1, 1),(1, 2, 1),(-1, 1, 1),(1, 0, 1)} obtenemos la matriz
+%H = [
+%-3 2.5 -1
+%-3 2 1
+%-3 0.5 1]
 
-P1 = [1, 1, -3];
-V1 = [1, 1, 0, -1];
-V2 = [1, 0, -1, 0];
+mOri = [1, 0, 1; 0, 1, 1; 0, 0, 0; 1, 1, 1];
+mCor = [2, 1, 1; 1, 2, 1; -1, 1, 1; 1, 0, 1];
 
-P2 = [1, 0, -1];
-P3 = [-3, -2, -3];
-
-MLInv = MultiplicarVectores(V1, V2) - MultiplicarVectores(V2, V1);
-LInv = ObtenerPlucker(MLInv);
-
-L = InvertirPlucker(LInv);
-
-ML = ObtenerMatrizPlucker(L);
-
-Plano = ML * [P1, 1]'
-
-%Plano = -2x -y -2z -3 = 0
-
-
-MLrecta = MultiplicarVectores([P2, 1], [P3, 1]) - MultiplicarVectores([P3, 1], [P2, 1]);
-
-Punto = MLrecta * Plano;
-
-Punto = Punto / 2
+H = GenerarHomografia(mOri, mCor)
